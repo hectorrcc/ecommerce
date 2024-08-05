a9bd7e9a7b21e069d6cfa28b6f31827f88090617
Hector Cabrera <hector.cabrera@bloxtek.com>
2024-08-05 08:16:25 -0300

    ajuste de auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,19 +1,5 @@
-import GoogleProvider from "next-auth/providers/google";
+import { authOptions } from "@/lib/authOptions";
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-
-export const authOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-  pages: {
-    signIn: "/admin",
-  },
-};
 
 const handler = NextAuth(authOptions);
 
